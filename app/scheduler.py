95f336ea2ccb9accd6d160655ac72973065e5c0c
Gunjan2222 <gunagarwal999@gmail.com>
2025-06-10 11:18:28 +0530

    schedule time changed

--- app/scheduler.py
+++ app/scheduler.py
@@ -4,6 +4,6 @@ from app import celery
 celery.conf.beat_schedule = {
     'daily-rent-notification': {
         'task': 'send_rent_notifications_task',
-        'schedule': crontab(hour=19, minute=2),
+        'schedule': crontab(hour=8, minute=0),
     },
 }
